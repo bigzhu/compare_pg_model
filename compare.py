9f511799f50f5eb141e5b9ae6582a0ee0b430563
bigzhu <vermiliondun@gmail.com>
2014-04-10 06:26:28 -0400

    可以对 表字段进行对比

--- compare.py
+++ compare.py
@@ -2,6 +2,10 @@
 # -*- coding: utf-8 -*-
 import ConfigParser
 import db
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+from utils import Storage
 
 
 def readDBInfo(db_cfg):
@@ -28,6 +32,15 @@ def getTableList(cfg, connect):
         l.append(i.tablename)
     return l
 
+
+def getTableColum(table_name, connect):
+    sql = '''
+        select format_type(a.atttypid,a.atttypmod) as type,a.attname as name
+        from pg_class as c,pg_attribute as a
+        where c.relname = '%s' and a.attrelid = c.oid and a.attnum>0 and a.atttypid<>0
+    ''' % table_name
+    return list(connect.query(sql))
+
 if __name__ == '__main__':
     cfg1 = readDBInfo('db1')
     cfg2 = readDBInfo('db2')
@@ -35,11 +48,16 @@ if __name__ == '__main__':
     connect2 = getConnect(cfg2)
     #for i in c1.query(sql):
     #    print i
+
+    #比较表差异
     tables1 = getTableList(cfg1, connect1)
     tables2 = getTableList(cfg2, connect2)
-    just1 = []
-    just2 = []
-    for i in (set(tables1) ^ set(tables2)):
+    just1 = [x for x in tables1 if x not in tables2]
+    just2 = [x for x in tables2 if x not in tables1]
+
+    diff_tables = just1 + just2
+    all_have_tables = [x for x in tables1 if x not in diff_tables]
+    for i in diff_tables:
         if i in tables1:
             just1.append(i)
         else:
@@ -54,3 +72,27 @@ if __name__ == '__main__':
     print '只有%s存在以下表:' % cfg2.get('ip')
     for i in just2:
         print i
+
+    #比较表字段
+    for table_name in all_have_tables:
+        table_colum1 = getTableColum(table_name, connect1)
+        table_colum2 = getTableColum(table_name, connect2)
+        just_colum1 = [x for x in table_colum1 if x not in table_colum2]
+        just_colum2 = [x for x in table_colum2 if x not in table_colum1]
+        if len(just_colum1) != 0:
+            print 'db %s 表 %s 独有:' % (cfg1.get('ip'), table_name)
+            for i in just_colum1:
+                print 'type:%s' % i.type, 'name:%s' % i.name
+        if len(just_colum2) != 0:
+            print 'db %s 表 %s 独有:' % (cfg2.get('ip'), table_name)
+            for i in just_colum2:
+                print 'type:%s' % i.type, 'name:%s' % i.name
+
+    #s = Storage()
+    #s.type = 'text'
+    #s.name = 'ip'
+    #s = dict(s)
+    #l = [s]
+    #base_mons = getTableColum('base_mon', connect1)
+    #base_mons = dict(base_mons)
+    #print set(l) ^ set(base_mons)
